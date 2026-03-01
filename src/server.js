e297def1b4d2bfc12fe2bb4883d78175446b96ab
alper kağan <kaganalperpamuk@gmail.com>
2026-03-01 21:35:50 +0300

    disable cors for mobile access

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import dotenv from "dotenv"
-import cors from 'cors'
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
@@ -8,19 +7,14 @@ import transactionsRoute from './routes/transactionsRoute.js';
 dotenv.config();
 const app = express()
 
-app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send("Wallet API is Live! 🚀");
-});
 
 const PORT = process.env.PORT || 5001;
 app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
-    app.listen(PORT, '0.0.0.0',() => {
+    app.listen(PORT,() => {
         console.log("Server is up and running ng PORT:", PORT);
     })
 })
