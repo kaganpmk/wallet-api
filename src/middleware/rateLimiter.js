93d7520189ee955855b1187acc0104cf760c9a56
alper kağan <kaganalperpamuk@gmail.com>
2026-03-02 01:52:43 +0300

    rate limiter debugged and codes saved from spagetti �

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -4,11 +4,8 @@ const rateLimiter = async(req,res,next) => {
     try {
         const identifier = req.headers['x-user-id'] || req.ip;
         const {success} = await ratelimit.limit(identifier)
-
-        console.log("Limiti Kontrol Edilen Kimlik:", identifier);
-
+        
         if(!success){
-            console.log("çok fazla")
             return res.status(429).json({
                 message:"Too many requests, please try again later"
             })
