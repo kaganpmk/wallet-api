9d3988d25457851cd5fd06fe968aa464cc326302
alper kağan <kaganalperpamuk@gmail.com>
2026-03-01 21:20:12 +0300

    fix: add cors and host for render2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
+import cors from 'cors'
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
