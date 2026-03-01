dd0575056b4074e9ba3747d8c5059ee4355b6e5a
alper kağan <kaganalperpamuk@gmail.com>
2026-03-01 21:19:26 +0300

    fix: add cors and host for render

--- src/server.js
+++ src/server.js
@@ -7,14 +7,19 @@ import transactionsRoute from './routes/transactionsRoute.js';
 dotenv.config();
 const app = express()
 
+app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Wallet API is Live! 🚀");
+});
 
 const PORT = process.env.PORT || 5001;
 app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
-    app.listen(PORT,() => {
+    app.listen(PORT, '0.0.0.0',() => {
         console.log("Server is up and running ng PORT:", PORT);
     })
 })
